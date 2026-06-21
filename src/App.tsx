8c7720cfda1a3488336b8cd27e7b983fd8cb22a7
Victor Mendes <victormendesdevv@gmail.com>
2026-06-20 22:15:30 -0300

    Input: usando condicionais no JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -19,6 +20,29 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='Task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo '
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>000000</p>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
